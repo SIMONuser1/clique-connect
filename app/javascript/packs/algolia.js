605930af04af42a3c25c6c9f4ee937e1d841fe41
Charly <charly.martin.1207@gmail.com>
2018-04-12 15:49:54 +1000

    algolia search working

--- app/javascript/packs/algolia.js
+++ app/javascript/packs/algolia.js
@@ -1,25 +1,59 @@
+import $ from 'jquery';
+
 // 1. Variables
 const client = algoliasearch("2NKIO6XW2P", "2f9755db01f6fd63288ea4daf832947f");
 const index = client.initIndex('Business');
 const searchOptions = { hitsPerPage: 10, page: 0 };
-const searchInput = "#algolia-search"
-let searchQuery = 'Glam'
+const searchInput = document.querySelector("#algolia-search")
 
 
 // 2. Functions
+
+function buildResults(r) {
+  console.log(r.name)
+}
+
 function searchDone(content) {
-  console.log(content)
+  const results = content.hits;
+  // console.log(results);
+  // results.forEach(r => buildResults(r))
+
+  searchInput.autocomplete({ hint: false }, [
+    {
+      source: $.fn.autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+      displayKey: 'my_attribute',
+      templates: {
+        suggestion: function(suggestion) {
+          return suggestion._highlightResult.my_attribute.value;
+        }
+      }
+    }
+  ]).on('autocomplete:selected', function(event, suggestion, dataset) {
+    console.log(suggestion, dataset);
+  });
+
 }
 
 function searchFailure(err) {
   console.error(err);
 }
 
-function initSearch() {
-  index.search(searchQuery, searchOptions)
+function initSearch(q) {
+  index.search(q, searchOptions)
     .then(content => searchDone(content))
     .catch(err => searchFailure(err));
 }
 
 
 // 3. Event Listeners
+searchInput.addEventListener("keyup", (e) => {
+  let searchQuery = e.target.value;
+
+  if (searchQuery.length > 3) {
+    initSearch(searchQuery);
+  }
+})
+
+
+
+
