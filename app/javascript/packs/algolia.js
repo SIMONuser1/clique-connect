a77b26a31b90530cce89fac5a0914d0ac93bad5b
Charly <charly.martin.1207@gmail.com>
2018-04-12 16:37:31 +1000

    Search injected in results container

--- app/javascript/packs/algolia.js
+++ app/javascript/packs/algolia.js
@@ -5,32 +5,26 @@ const client = algoliasearch("2NKIO6XW2P", "2f9755db01f6fd63288ea4daf832947f");
 const index = client.initIndex('Business');
 const searchOptions = { hitsPerPage: 10, page: 0 };
 const searchInput = document.querySelector("#algolia-search")
+const searchResults = document.querySelector("#search-results-list")
 
 
 // 2. Functions
 
 function buildResults(r) {
-  console.log(r.name)
+  const item = `<a href="#" class="list-group-item list-group-item-action">${r.name}</a>`
+  // `<li class="list-group-item">${r.name}</li>`
+  searchResults.insertAdjacentHTML("beforeend", item)
+}
+
+function clearResults() {
+  searchResults.innerHTML = "";
 }
 
 function searchDone(content) {
   const results = content.hits;
-  // console.log(results);
-  // results.forEach(r => buildResults(r))
-
-  searchInput.autocomplete({ hint: false }, [
-    {
-      source: $.fn.autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-      displayKey: 'my_attribute',
-      templates: {
-        suggestion: function(suggestion) {
-          return suggestion._highlightResult.my_attribute.value;
-        }
-      }
-    }
-  ]).on('autocomplete:selected', function(event, suggestion, dataset) {
-    console.log(suggestion, dataset);
-  });
+  console.log(results);
+  clearResults()
+  results.forEach(r => buildResults(r))
 
 }
 
@@ -53,6 +47,16 @@ searchInput.addEventListener("keyup", (e) => {
     initSearch(searchQuery);
   }
 })
+
+searchInput.addEventListener("blur", clearResults);
+
+searchInput.addEventListener("focus", (e) => {
+  let searchQuery = e.target.value;
+
+  if (searchQuery.length > 3) {
+    initSearch(searchQuery);
+  }
+});
 
 
 
