1c25891163e548001acc87eeca7ece133ab5a10e
Blake Rowley <blakehenryrowley@gmail.com>
2018-04-13 12:45:10 +1000

    WIP of Show for Business

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  # def after_sign_in_path_for(resource)
+  #  #path_to_redirect_to
+  # end
+
+  # def after_sign_up_path_for(resource)
+  #  #path_to_redirect_to
+  # end
 end
