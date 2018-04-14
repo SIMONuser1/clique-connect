c48cbc58c8de30f704b13c993ac9c283415a30d9
Blake Rowley <blakehenryrowley@gmail.com>
2018-04-14 19:39:00 +1000

    A bit of this and that

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,6 +1,6 @@
 class SuggestionsController < ApplicationController
   def index
-    #@suggestions = current_user.business.suggestions
+    # @suggestions = current_user.suggestions
   end
 
   def update
