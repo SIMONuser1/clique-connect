90b3f9abe0e1ce4de94c2519f79e1611eeed86cb
Charly <charly.martin.1207@gmail.com>
2018-04-16 10:59:51 +1000

    browser auto complete off + result position set on search input.

--- app/javascript/packs/algolia.js
+++ app/javascript/packs/algolia.js
@@ -4,8 +4,9 @@ import $ from 'jquery';
 const client = algoliasearch("2NKIO6XW2P", "2f9755db01f6fd63288ea4daf832947f");
 const index = client.initIndex('Business');
 const searchOptions = { hitsPerPage: 10, page: 0 };
-const searchInput = document.querySelector("#algolia-search")
-const searchResults = document.querySelector("#search-results-list")
+const searchInput = document.querySelector("#algolia-search");
+const resultContainer = document.querySelector("#search-results-container");
+const resultList = resultContainer.querySelector(".list-group");
 
 
 // 2. Functions
@@ -13,13 +14,31 @@ const searchResults = document.querySelector("#search-results-list")
 function buildResults(r) {
   const item = `<a href="#" class="list-group-item list-group-item-action">${r.name}</a>`
   // `<li class="list-group-item">${r.name}</li>`
-  searchResults.insertAdjacentHTML("beforeend", item)
+  resultList.insertAdjacentHTML("beforeend", item)
 }
 
 function clearResults() {
-  searchResults.innerHTML = "";
+  resultList.innerHTML = "";
 }
 
+function computeNavPosition() {
+  const pos = searchInput.getBoundingClientRect();
+  return {
+    bottom: pos.bottom,
+    right: (window.innerWidth - pos.width - pos.left),
+    left: pos.left
+  }
+}
+
+function updateResultPotition(p) {
+  resultContainer.style.setProperty('top', `${p.bottom + 10}px`);
+  resultContainer.style.setProperty('left', `${p.left}px`);
+  resultContainer.style.setProperty('right', `${p.right}px`);
+  resultContainer.style.setProperty('height', '20px');
+  resultContainer.style.setProperty('background', 'red')
+}
+
+// Algolia methods
 function searchDone(content) {
   const results = content.hits;
   console.log(results);
@@ -37,15 +56,21 @@ function initSearch(q) {
     .then(content => searchDone(content))
     .catch(err => searchFailure(err));
 }
+// Algolia methods (End)
+
 
 
 // 3. Event Listeners
 searchInput.addEventListener("keyup", (e) => {
   let searchQuery = e.target.value;
+  let pos = computeNavPosition();
+  updateResultPotition(pos);
 
   if (searchQuery.length > 3) {
     initSearch(searchQuery);
   }
+
+  console.log(pos);
 })
 
 searchInput.addEventListener("blur", clearResults);
@@ -57,7 +82,5 @@ searchInput.addEventListener("focus", (e) => {
     initSearch(searchQuery);
   }
 });
-
-
 
 
